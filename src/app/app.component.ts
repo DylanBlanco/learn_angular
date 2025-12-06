9cfa42e82f5eb396da866c937a2798434e1afcc2
DylanBlanco <dylanblanco1198@gmail.com>
2025-12-06 14:50:55 +0100

    array - SIGNALS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { sign } from 'node:crypto';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learn-angular';
+  title = 'App Angular';
+
+    x = 5;
+    y = 15;
+
+    students = [
+        {id: 1, nome: 'Marco', voto: 3},
+        {id: 2, nome: 'Valeria', voto: 8},
+        {id: 3, nome: 'Luca', voto: 4},
+        {id: 4, nome: 'Luca', voto: 5},
+        {id: 5, nome: 'Giulia', voto: 6},
+        {id: 6, nome: 'Giulia', voto: 3}
+    ]
+
+    // Proprietà signal
+    counter = signal(0);
+
+    increaseCounter() {
+        this.counter.update(v => v+1);  // UPDATE: per modificare il valore della signal sulla base del valore precedente
+    }
+    decreaseCounter() {
+        this.counter.update(v => v-1);
+    }
+    resetCounter() {
+        this.counter.set(0);
+    }
+    setCounter(v: number) {
+        this.counter.set(v);
+    }
+
+    professors = signal([
+        {id: 1, nome: 'Prof. Bianchi', materia: 'Matematica'},
+        {id: 2, nome: 'Prof.ssa Rossi', materia: 'Italiano'},
+        {id: 3, nome: 'Prof. Verdi', materia: 'Inglese'}
+    ])
 }
